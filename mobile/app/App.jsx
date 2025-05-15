73478c781f2f576db6239ea0833327de7d564afb
James Ricarte <uhenyou@gmail.com>
2025-05-15 15:39:11 +0800

    Fixed fonts onload before hiding splash screen

--- mobile/app/App.jsx
+++ mobile/app/App.jsx
@@ -1,8 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import MainStackNavigator from "./navigation/MainStackNavigator";
 import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 import "../global.css";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,8 +15,18 @@ export default function App() {
     "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
   });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onLayoutRootView}>
       <MainStackNavigator />
     </NavigationContainer>
   );
